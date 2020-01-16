94248cbf100192311e22279c3963d0fa47497aa7
Philip (flip) Kromer <flip@mrflip.com>
2020-01-16 17:38:50 -0600

    initial commit

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">My Vue Native App</text>
+  </view>
+</template>
+
+<script>
+import React from "react";
+const __react__vue__options = {};
+import {YellowBox} from 'react-native'
+YellowBox.ignoreWarnings(['has been merged']);
+
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
